9576f90d70b8463cd3ec0cc3453e190e4035448b
cyb <1179506440@qq.com>
2019-04-17 23:26:29 +0800

    修改https

--- sever.js
+++ sever.js
@@ -6,16 +6,28 @@ import graphqlHTTP  from 'express-graphql'
 import {schema} from './schema'
 let app = express();
 let PORT = 3000;
+import https from 'https'
+import fs from 'fs';
 
+let option={
+    key:fs.readFileSync('./ssl/2026737.key'),
+    cert:fs.readFileSync('./ssl/2026797.pem'),
+};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql:true
 }));
 
-let server = app.listen(PORT, function() {
+// let server = app.listen(PORT, function() {
+//     let host = server.address().address;
+//     let port = server.address().port;
+//     console.log(server.address());
+//     console.log('GraphQL listening at http://%s:%s', host, port);
+// });
+let server=https.createServer(option,app).listen(PORT,function () {
     let host = server.address().address;
     let port = server.address().port;
     console.log(server.address());
-    console.log('GraphQL listening at http://%s:%s', host, port);
+    console.log('GraphQL listening at https://%s:%s', host, port);
 });
